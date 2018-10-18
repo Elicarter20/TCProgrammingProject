9e69fb64c5b19bbc28f63a682a5815067dd1010e
Elicarter20 <elicarter20@gmail.com>
2018-10-17 21:30:11 -0400

    Update translator.h

--- translator.h
+++ translator.h
@@ -16,22 +16,6 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define FORMAT_ONE_TYPE 0
-#define FORMAT_TWO_TYPE 1
-#define FORMAT_ONE_NUM_SIZE 2 // bytes
-#define FORMAT_TWO_NUM_SIZE 5 // characters
-#define FORMAT_ONE_AMOUNT_SIZE 1 // bytes
-#define FORMAT_TWO_AMOUNT_SIZE 3 // bytes
-#define NO_BYTES_READ -1
-#define SUCCESS_MESSAGE "Success"
-#define ERROR_MESSAGE "Format error"
-#define NO_FLAGS 0
-#define NO_TRANSLATION 0
-#define FORMAT_ONE_TO_TWO 1
-#define FORMAT_TWO_TO_ONE 2
-#define SWAP_FORMATS 3
-#define OUTPUT_FOLDER "../testData/";
-
 char* get_values(char* input);
 int check_file(char* input);
 char* change_type(char* values, char* tf);
